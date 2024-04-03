66ff8a6bc5bdcb7e5fb8c4c49cd1c34b3c046482
Nguyen Quyen <nlnq2511@gmail.com>
2024-04-03 20:46:42 +0700

    lesson8

--- Assets/MyGame/Scrips/PlayerManager.cs
+++ Assets/MyGame/Scrips/PlayerManager.cs
@@ -1,93 +1,24 @@
-﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
-    public float moveSpeed = 2f;
-    public Animator playerAnimator;
-    private bool IsWalking = false;
-    private bool IsRunning = false;
-    private bool IsJumping = false;
-
+    [SerializeField] float horizontalSpeed = 100f;
+    [SerializeField] float verticalSpeed = 10f;
+    [SerializeField] private Animator _anim;
+    // Start is called before the first frame update
     void Start()
     {
-        playerAnimator = GetComponent<Animator>();
     }
 
+    // Update is called once per frame
     void Update()
     {
-        // Kiểm tra và cập nhật trạng thái di chuyển
-        UpdateMovement();
-
-        // Kiểm tra và cập nhật trạng thái chạy
-        UpdateRunning();
-
-        // Kiểm tra và cập nhật trạng thái nhảy
-        UpdateJumping();
-    }
-
-    void UpdateMovement()
-    {
-        // Di chuyển trước
-        if (Input.GetKey("w"))
-        {
-            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
-            IsWalking = true;
-        }
-        // Di chuyển sau
-        else if (Input.GetKey("s"))
-        {
-            transform.Translate(Vector3.back * moveSpeed * Time.deltaTime);
-            IsWalking = true;
-        }
-        // Di chuyển phải
-        else if (Input.GetKey("d"))
-        {
-            transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
-            IsWalking = true;
-        }
-        // Di chuyển trái
-        else if (Input.GetKey("a"))
-        {
-            transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
-            IsWalking = true;
-        }
-        else
-        {
-            IsWalking = false;
-        }
-
-        // Cập nhật trạng thái di chuyển trong Animator
-        playerAnimator.SetBool("IsWalking", IsWalking);
-    }
-
-    void UpdateRunning()
-    {
-        // Kiểm tra và cập nhật trạng thái chạy
-        if (Input.GetKey("left shift"))
-        {
-            IsRunning = true;
-        }
-        else
-        {
-            IsRunning = false;
-        }
-
-        // Cập nhật trạng thái chạy trong Animator
-        playerAnimator.SetBool("IsRunning", IsRunning);
-    }
-    void UpdateJumping()
-    {
-        // Kiểm tra và cập nhật trạng thái chạy
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            IsJumping = true;
-        }
-        else
-        {
-            IsJumping = false;
-        }
-
-        // Cập nhật trạng thái chạy trong Animator
-        playerAnimator.SetBool("IsJumping", IsJumping);
+        float h = horizontalSpeed * Input.GetAxis("Horizontal") * Time.deltaTime;
+        float v = verticalSpeed * Input.GetAxis("Vertical") * Time.deltaTime;
+        transform.Translate(0, 0, v);
+        transform.Rotate(0, h, 0);
+        _anim.SetFloat("IsWalking",Mathf.Abs(v));
     }
 }
